c9aa25b5c58c4e555984ee495642d64a7e3c04a3
daenerys <pereirajefferson06@gmail.com>
2017-10-28 23:00:25 -0200

    Criando as funcionalidades de soma e inserção de números

--- app/src/main/java/com/example/daenerys/caculadora/Calculadora.java
+++ app/src/main/java/com/example/daenerys/caculadora/Calculadora.java
@@ -1,5 +1,6 @@
 package com.example.daenerys.caculadora;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +13,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class Calculadora extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+        String resposta = "";
+        TextView tela;
+        Integer resultado;
+        String operando = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,9 @@ public class Calculadora extends AppCompatActivity
         setContentView(R.layout.activity_calculadora);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        tela = (TextView) findViewById(R.id.textViewResultado);
+        resultado = 0;
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -34,9 +44,92 @@ public class Calculadora extends AppCompatActivity
     }//onCreate
 
 
+ //Códigos funcionalidade da calculadora
+
+    public void um(View v){
+        resposta +="1";
+        tela.setText(resposta);
+    }//um
+
+    public void dois(View v){
+        resposta +="2";
+        tela.setText(resposta);
+    }//dois
+
+    public void tres(View v){
+        resposta +="3";
+        tela.setText(resposta);
+    }//tres
+
+    public void quatro(View v){
+        resposta +="4";
+        tela.setText(resposta);
+    }//quatro
+
+    public void cinco(View v){
+        resposta +="5";
+        tela.setText(resposta);
+    }//cinco
+
+    public void seis(View v){
+        resposta +="6";
+        tela.setText(resposta);
+    }//seis
+
+    public void sete(View v){
+        resposta +="7";
+        tela.setText(resposta);
+    }//sete
+
+    public void oito(View v){
+        resposta +="8";
+        tela.setText(resposta);
+    }//oito
+
+    public void nove(View v){
+        resposta +="9";
+        tela.setText(resposta);
+    }//nove
+
+    public void zero(View v){
+        resposta +="0";
+        tela.setText(resposta);
+    }//nove
 
 
+    //Operações//-------------------------------------------------------
 
+    public void somar(View v){
+        tela.setText("");
+        operando = "+";
+        resultado = Integer.parseInt(resposta);
+        resposta = "";
+    }//somar
+
+    public void subtrair(View v){
+        tela.setText("");
+        resposta = "";
+    }//subtrair
+
+    public void multiplicar(View v){
+        tela.setText("");
+        resposta = "";
+    }//multiplicar
+
+    public void dividir(View v){
+        tela.setText("");
+        resposta = "";
+    }//dividir
+
+    public void igual(View v){
+        if(operando.equals("+")){ //usar um case para operandos retratar o igual para cada operando
+            resposta = tela.getText().toString();
+            resultado += Integer.parseInt(resposta); //ifeal tirar essa funcionalidade para um método
+                                                    //que recebe um operando
+        }
+
+        tela.setText(String.valueOf(resultado.toString()));
+    }//igual
 
 
  //Parte responsável para o uso do Drawermenu//------------------------
